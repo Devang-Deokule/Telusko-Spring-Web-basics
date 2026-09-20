316a6d77346655600e48c4e7562e331b051236bb
Devang-Deokule <devangdeokule26@gmail.com>
2026-09-20 23:26:03 +0530

    displaying data using httpsession

--- SpringBootWeb1/src/main/java/com/devang/SpringBootWeb1/HomeController.java
+++ SpringBootWeb1/src/main/java/com/devang/SpringBootWeb1/HomeController.java
@@ -2,6 +2,7 @@ package com.devang.SpringBootWeb1;
 
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -15,11 +16,13 @@ public class HomeController {
     }
 
     @RequestMapping("add")
-    public String add(HttpServletRequest req){
+    public String add(HttpServletRequest req, HttpSession session){
 
         int num1 = Integer.parseInt(req.getParameter("num1"));
         int num2 = Integer.parseInt(req.getParameter("num2"));
         int result = num1 + num2;
+
+        session.setAttribute("result", result);
 
         return "result";
     }
--- SpringBootWeb1/src/main/webapp/result.jsp
+++ SpringBootWeb1/src/main/webapp/result.jsp
@@ -7,7 +7,6 @@
     </body>
         <h2>Devang Calculator</h2>
 
-         <h2>Result is: </h2>
-
+         <h2>Result is: <%= session.getAttribute("result") %></h2>
     </body>
 </HTML>
